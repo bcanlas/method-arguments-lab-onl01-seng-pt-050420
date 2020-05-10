cccd5330254382e62b9d532c9456c1a163e00cc9
Bianca Canlas <bcanlas87@gmail.com>
2020-05-10 21:45:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,4 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction_with_language(name, languag)
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
-
-introduction("Josh")
-
-
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}.
-end
-introduction_with_language(Josh, JS)
+introduction_with_language("Josh", "JS")
